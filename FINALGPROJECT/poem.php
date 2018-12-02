d92333f9a3c5ea6b5b8f5c1e76e5128f816fc463
Kyle Lekkas <35651136+Kleonlekkas@users.noreply.github.com>
2018-12-02 18:41:53 -0500

    Added actual poem to Alma Mater

--- FINALGPROJECT/poem.php
+++ FINALGPROJECT/poem.php
@@ -1,12 +1,31 @@
 <?php
 $page = "Poem";
 include 'assets/inc/header.php';
-session_start();
 ?>
 <body>
     <img class = "toppic" src = "assets/img/alma_mater.png" alt="Learnel Kernel Poem">
     <div class = "content">
-        qwertyuiopasdfghjklzxcvbnm qwertyuiopasdfghjklzxcvbnm qwertyuiopasdfghjklzxcvbnm qwertyuiopasdfghjklzxcvbnm, qwertyuiosdfghjklzxcvbnm
+        <p class="sec1">So you've come here cuz you wanna learn Linux,</p>
+        <p class="sec1">A topic boring nuff to send you to clinics.</p>
+        
+        <p class="sec2">But fear not friend, for it can be fun!</p>
+        <p class="sec2">At the Learnel Kernel, it can be done.</p>
+
+        <br />
+        
+        <p class="sec3">On this site, we'll have all that you'll need.</p>
+        <p class="sec3">A place you can learn, a place you can read.</p>
+        
+        <p class="sec4">When all's said and done, you'll be a master.</p>
+        <p class="sec4">At the Learnel Kernel, it couldn't be faster.</p>
+        
+        <br />
+        
+        <p class="sec5">Wasn't that easy? We didn't pretend!</p>
+        <p class="sec5">Through your hard work our lessons come to an end</p>
+        
+        <p class="sec6">You're a Linux pro now, why not say it louder?</p>
+        <p class="sec6">At the Learnel Kernel, we couldn't be prouder.</p>        
     </div>
 </body>
 <?php
